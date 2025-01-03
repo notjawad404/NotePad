567e639987b264067310550b15ab261e41f780b8
notjawad404 <jawadali000123@gmail.com>
2025-01-03 22:56:15 +0500

    add get notes by id route

--- Backend/routes/noteRoutes.js
+++ Backend/routes/noteRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createNote, getNotes, updateNote, deleteNote } = require('../controllers/noteController');
+const { createNote, getNotes, getNoteById, updateNote, deleteNote } = require('../controllers/noteController');
 
 const router = express.Router();
 
 router.post('/', createNote);
 router.get('/', getNotes);
+router.get('/:id', getNoteById);
 router.put('/:id', updateNote);
 router.delete('/:id', deleteNote);
 
